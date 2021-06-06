b362e9bef6ddfea9a693d5072190897babdfdb3d
kianahs <47052269+kianahs@users.noreply.github.com>
2021-06-06 21:56:38 +0430

    calculate membership and update centroids done
    1-D data set

--- fuzzy.py
+++ fuzzy.py
@@ -1,38 +1,67 @@
 import numpy as np
 from numpy import genfromtxt
-
+import matplotlib.pyplot as plt
 
 def read_dataset(filename):
     data = genfromtxt(filename, delimiter=',')
     # print(data.shape)
     data = data.flatten()
     # print(data.shape)
-    # print(data)
+    print(data)
     return data
 
 
+def calculate_membership_update_centroids(c,data,centroids,m,u):
+    for r in range(100):
+        flag = 0
+        for i in range(int(c)):
+            for k in range(np.size(data)):
+                sigma = 0
+                for j in range(int(c)):
+                    if data[k] != centroids[i] and data[k] != centroids[j]:
+                        sigma += pow(abs(data[k] - centroids[i]) / abs(data[k] - centroids[j]), (2 / (m - 1)))
+                    elif data[k] == centroids[i]:
+                        sigma = 1
+                    elif data[k] != centroids[j]:
+                        flag = 1
+                if flag == 0:
+                    u[i][k] = 1 / sigma
+                else:
+                    u[i][k] = 0
+                    flag = 0
+
+        for n in range(int(c)):
+            nominator = 0
+            denominator = 0
+            for t in range(np.size(data)):
+                # print("hi")
+                nominator += pow(u[n][t], m) * data[t]
+                denominator += pow(u[n][t], m)
+                centroids[n] = nominator / denominator
+
+
+def calculate_costs(data,c,u,m,centroids):
+    # cost calculation
+    item_cost = 0
+    for j in range(np.size(data)):
+        for i in range(int(c)):
+            item_cost += pow(u[i][j], m) * pow((data[j] - centroids[i]), 2)
+
+    return item_cost
+
 def main():
     data = read_dataset('data1.csv')
-    print("Enter number of clusters")
-    c = input()
-    m = 2
-    centroids = np.random.choice(data.shape[0], int(c), replace=False)
-    v = np.zeros(shape=(np.size(data),int(c)))
-    u = np.ones(shape=(int(c), np.size(data)))
-    cal = np.zeros(shape=(int(c), np.size(data)))
-    # print(v.shape)
-    for i in range(int(c)):
-        v[:, i] = data[centroids[i]]
+    costs = []
+    for p in range(0, 7):
+        c = p
+        m = 3
+        centroids = np.random.choice(data.shape[0], int(c), replace=False)
+        u = np.ones(shape=(int(c), np.size(data)))
+        calculate_membership_update_centroids(c, data, centroids, m, u)
+        costs.append(calculate_costs(data, c, u, m, centroids))
 
-    # print(v.shape)
-    # print(v)
-    for i in range(int(c)):
-        for j in range(int(c)):
-            cal[i, :] += np.transpose(np.power(np.divide(np.subtract(data, v[:, i]), np.subtract(data, v[:, j])), (2/(m-1))))
-
-    u = np.divide(u,cal)
-
-
+    plt.plot(costs)
+    plt.show()
 
 if __name__ == '__main__':
     main()
